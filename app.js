c1601608f33820c725fafbd0b520bd5d47e76753
bidanhbido <bidanh@gomeu.com>
2021-04-29 19:54:33 +0700

    Hoan thanh tao moi trang about-us, thieu phan css danh cho slider

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use('view engine', 'ejs');
+app.use('views', 'views');
+
 app.get('/index', function (req, resp){
     resp.send(`Hello World`);
 })
